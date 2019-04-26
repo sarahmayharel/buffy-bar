9a4b36647559b228001a6189908909d34fbf8ba6
sarahmayharel <sarahmay@workman.com>
2019-04-26 13:02:47 -0400

    adding main.js file

---
+++ main.js
@@ -0,0 +1,10 @@
+         // Initialize Firebase
+         var config = {
+           apiKey: "AIzaSyCQ660GP-4AYpiW56MAezG7eT94Q3nt4VI",
+           authDomain: "buffy-bar.firebaseapp.com",
+           databaseURL: "https://buffy-bar.firebaseio.com",
+           projectId: "buffy-bar",
+           storageBucket: "buffy-bar.appspot.com",
+           messagingSenderId: "434567988048"
+         };
+         firebase.initializeApp(config);
